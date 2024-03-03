40f903ded1e4723b0b1eb296f9955b9e1b834565
Obed <obedmontano43@gmail.com>
2024-03-03 10:10:14 -0500

    removed unnecessary uuid reference.

--- src/main/java/obed/me/lccommons/api/entities/groups/RankInfo.java
+++ src/main/java/obed/me/lccommons/api/entities/groups/RankInfo.java
@@ -3,11 +3,9 @@ package obed.me.lccommons.api.entities.groups;
 import lombok.Data;
 
 import java.time.Instant;
-import java.util.UUID;
 
 @Data
 public class RankInfo {
-    private UUID uuid;
     private Rank rank;
     private boolean permanent;
     private boolean hide;
